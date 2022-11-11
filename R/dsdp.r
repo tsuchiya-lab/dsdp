5a37989b1ff311a3fd353477e3f05569dc0da5f9
tsuchiya-lab <skakihara@yahoo.com>
2022-11-11 13:39:42 +0900

    dsdp.r description

--- R/dsdp.r
+++ R/dsdp.r
@@ -1,6 +1,6 @@
 #' dsdp: Density Estimation using Semidefinite Programming
 #'
-#' @description Density estimation with Semidefinite Programming.
+#' Density estimation with Semidefinite Programming.
 #' The models of probability density functions are Gaussian or exponential
 #' distributions with polynomial correction terms. Using a maximum likelihood
 #' method, it computes parameters of Gaussian or exponential distributions
